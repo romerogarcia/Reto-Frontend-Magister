e04dfd3dc57aaba428a840fa4aaf3a648749316f
Noelia R <noeliaromga@gmail.com>
2022-08-16 13:43:26 +0200

    resolve errors with landing, delete mainlanding and create a new landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,33 @@
-import Mainlanding from './Mainlanding';
+import { Link } from 'react-router-dom';
+
 function Landing() {
+  const handleClickStart = () => {};
   return (
     <>
       <div className="Landing">
-        <Mainlanding path="/Mainlanding" />
+        <main className="landing">
+          <nav className="menu-burguer">
+            <i className=" fa-solid fa-bars fa-4x  "></i>
+          </nav>
+          <section className="section">
+            <div className="section__elements">
+              <i className="fa-solid fa-landmark fa-4x "></i>
+              <h1 className="section__elements__title">Magister</h1>
+            </div>
+            <p className="section__subtitle">¡Comencemos con tu matrícula!</p>
+            <p className="section__text">
+              Para comenzar a especializarte, vamos a realizar unas preguntas
+              para darte el mejor servicio
+            </p>
+
+            <button className="section__button" onClick={handleClickStart}>
+              <Link className="section__button__link" to="/specialized">
+                Comenzar
+              </Link>
+            </button>
+            <p className="section__back">Volver atrás</p>
+          </section>
+        </main>
       </div>
     </>
   );
